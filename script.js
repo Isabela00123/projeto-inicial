e2bc31b80ee9e43f1cc5bed078e0b9f5b425a827
isabela00123 <isabeladiasmendes34@gmail.com>
2024-03-19 08:55:18 -0300

    formulário

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
 var nome = document.getElementById("nome");
+var email = document.getElementById("email");
+var telefone = document.getElementById("telefone");
+var CEP = document.getElementById("CEP");
+var logradouro = document.getElementById("logradouro");
+var numero = document.getElementById("numero");
+var complemento = document.getElementById("complemento");
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
+
+
+
+
+var saida = document.getElementById("saida-de-dados");
 
 function alertar(event){
-   alert("Você  clicou no botão!!! " + nome.value);
+   //alert("Você  clicou no botão!!! " + nome.value);
+
+   saida.innerText = "Nome: " + nome.value +
+
+                     "\n email: " + email.value +
+                     "\n telefone: " + telefone.value+
+                     "\n CEP: " + CEP.value+
+                     "\n logradouro: " + logradouro.value+
+                     "\n numero: " + numero.value+
+                     "\n complemento: " + complemento.value+
+                     "\n bairro: " + bairro.value+
+                     "\n cidade: " + cidade.value+
+                     "\n estado: " + estado.value;
+
+
+
+
 }
